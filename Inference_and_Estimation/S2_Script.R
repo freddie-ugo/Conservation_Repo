1dfe3879870a9189c56d6f5b12e62ee1d81dbe53
freddie-ugo <freddieugo@gmail.com>
2024-01-24 16:40:19 +0000

    Session 3 Completed

--- Inference_and_Estimation/S2_Script.R
+++ Inference_and_Estimation/S2_Script.R
@@ -1,8 +1,12 @@
+setwd('/Users/freddieugo/Documents/GitHub/Conservation_Repo/Inference_and_Estimation')
 # Read in CSV
 #Fixes header issue with skip
 dbdata_DA <- read.csv("S2_Prac/dbdata_DA.csv", skip = 1)
 
 # Load Libraries
+install.packages('tidyr')
+install.packages('dplyr')
+install.packages('stringr')
 library(tidyr)
 library(dplyr)
 library(stringr)
@@ -21,16 +25,64 @@ dbdata_DA %>% pull(taxon) %>% unique() %>% sort()
 dbdata_DA %>% pull(number) %>% range()
 dbdata_DA %>% pull(number) %>% summary()
 
+
+# Fix DA Data -------------------------------------------------------------
+
 # FIX SPECIES MISSPELLING ISSUE
-dbdata_DA %>% 
+dbdata_DA_fix <- dbdata_DA %>% 
   mutate(taxon = str_replace_all(taxon, "  ", " ")) %>%
   mutate(taxon = str_replace_all(taxon, "[ \t]+", " ")) %>%
   mutate(taxon = str_replace_all(taxon, "Eurystenus", "Eurysternus")) %>%
-  mutate(taxon = str_replace_all(taxon, "belorinus", "belhorinus")) %>%
-  count(taxon)
+  mutate(taxon = str_replace_all(taxon, "belorinus", "belhorinus"))
 # Got whole table, turned taxon into taxon where the strings of two spaces were replaced with one space.
  
 #FIX TRANSECT AND SITE
 dbdata_DA %>%
   unite("site", transect, site, sep = "_")
+
+#Session 3
+
+# Fix BA data -------------------------------------------------------------
+
+dbdata_BA <- read.csv("S2_Prac/dbdata_BA.csv")
+dbdata_BA
+
+str(dbdata_BA)
+
+#Check unique sites
+dbdata_BA %>% pull(site) %>% unique
+
+#Fix data
+dbdata_BA_fix <- dbdata_BA %>%
+  mutate(camp = "BA",
+         #Missing Dates to NA
+         setdate = ifelse(setdate == "", NA, setdate),
+         collectdate = ifelse(collectdate == "", NA, collectdate)) %>%
+  # Fill down missing dates
+  fill(setdate, collectdate, .direction = "down") %>%
+  # Convert to Long format
+  pivot_longer(!c(camp, site, setdate, collectdate, name), names_to = "taxon", values_to = "count") %>%
+  # Correct missing counts
+  mutate(count = replace_na(count, 0))
+
+# Relocate
+dbdata_BA_fix %>% relocate(count, site)
+
+#Filter selects rows
+dbdata_BA_fix %>% filter(taxon == "Copris.laeviceps")
+dbdata_BA_fix %>% filter(site == "BA2_SS1")
+dbdata_BA_fix %>% filter(site != "BA1_SS1")
+
+#Select selects Columns
+dbdata_BA_fix %>% select(site, taxon, count)
+dbdata_BA_fix %>% select(-collectdate, -camp)
+
+#Joining data
+# Fix CO data -------------------------------------------------------------
+
+dbdata_CO_occ <- read.csv("S2_Prac/dbdata_CO_occ.csv")
+dbdata_CO_rec <- read.csv("S2_Prac/dbdata_CO_rec.csv")
+
+left_join(dbdata_CO_occ, dbdata_CO_rec, by = c("id" = "id"))
+
 
---
+++ Inference_and_Estimation/S3_Script.R
@@ -0,0 +1,91 @@
+library(tidyr)
+library(dplyr)
+library(stringr)
+install.packages("readxl")
+library(readxl)
+install.packages("ggplot2")
+library(ggplot2)
+
+
+# Load Data ---------------------------------------------------------------
+
+dbdata <- read_excel("S3_Prac/dbanalysis.xlsx", sheet = "dbdata")
+habitat <- read_excel("S3_Prac/dbanalysis.xlsx", sheet = "habitat")
+remotesense <- read_excel("S3_Prac/dbanalysis.xlsx", sheet = "remotesense")
+
+
+# Fix UTM in habitat ------------------------------------------------------
+
+str(habitat)
+str(remotesense)
+
+habitat <- habitat %>%
+  mutate(UTM_X = as.numeric(UTM_X),
+         UTM_Y = as.numeric(UTM_Y))
+
+
+# Merging Data ------------------------------------------------------------
+
+main <- left_join(dbdata, habitat, by = c("site" = "site")) %>%
+  left_join(remotesense, by = c("UTM_X" = "UTM_X", "UTM_Y" = "UTM_Y"))
+
+
+# How does aspect affect the number of Copris nubilosus? ------------------
+
+main %>%
+  filter(taxon == "Copris nubilosus") %>%
+  ggplot(aes(x = aspect, y = count,)) +
+  geom_point(shape = 1, col = "green")
+
+# How does elevation affect the abundance of Dichotomius species?
+#Separate by colour
+main %>%
+  filter(str_detect(taxon, "Dichotomius")) %>%
+  ggplot(aes(x = dem, y = count, colour = taxon)) +
+  geom_point()
+
+#Separate by panel
+main %>%
+  filter(str_detect(taxon, "Dichotomius")) %>%
+  ggplot(aes(x = dem, y = count, colour = taxon)) +
+  geom_point() +
+  facet_wrap(~taxon)
+
+#Separate by shape
+main %>%
+  filter(str_detect(taxon, "Dichotomius")) %>%
+  ggplot(aes(x = dem, y = count, shape = taxon)) +
+  geom_point()
+
+
+# Line Plot (will look ugly) ----------------------------------------------
+
+main %>%
+  filter(str_detect(taxon, "Dichotomius")) %>%
+  ggplot(aes(x = dem, y = count, colour = taxon)) +
+  geom_point() +
+  geom_line()
+
+
+# Bar Chart ---------------------------------------------------------------
+
+# How many individuals for each species?
+
+main %>% 
+  group_by(taxon) %>%
+  summarise(total = sum(count)) %>%
+  ggplot(aes(x = taxon, y = total)) +
+  geom_col() +
+  coord_flip()
+
+
+# Histogram ---------------------------------------------------------------
+
+# What is the distribution of the counts of Eurysternus magnus
+main %>%
+  filter(taxon == "Eurysternus magnus") %>%
+  ggplot(aes(x = count)) +
+  geom_histogram(bins = 50, fill = "yellow") +
+  labs(y = "frequency")
+  
+
